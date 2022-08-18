12f5cde5d44da55ad4a7128e5700fff4351f7d97
amannitp <amannitp272@gmail.com>
2022-08-19 04:12:36 +0530

    log in page finish

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,9 +5,32 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import instagramLogo from './Assets/Instagram.JPG'
+import {createUseStyles} from 'react-jss'
+import TextField from '@mui/material/TextField';
+import Alert from '@mui/material/Alert';
 
 import './signup.css'
+import { red } from "@mui/material/colors";
+import { height } from "@mui/system";
 export default function SignUp() {
+
+
+  const useStyle=createUseStyles({
+             text:{
+              margin:'2% 0'
+
+             },
+             margin:{
+              marginTop:'4%',
+             },
+             card2:{
+              marginTop:'6%',
+              padding:"2% 0"
+             }
+  })
+
+  const classes=useStyle()
+  
   return (
     <div className="signupWrapper">
       <div className="signupCard">
@@ -16,19 +39,29 @@ export default function SignUp() {
             <img src={instagramLogo}/>
           </div>
           <CardContent>
-            <Typography align='right' variant='string'>
-              Aman Raj
+            <Typography a variant='subtitle1' className={classes.text}>
+              sign up to see photo and video from your friend
             </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+            {true &&  <Alert severity="error">This is an error alert — check it out!</Alert>}
+            <TextField id="outlined-basic" label="Email" variant="outlined" margin="dense" fullWidth={true} />
+            <TextField id="outlined-basic" label="Password" variant="outlined" margin='dense' fullWidth='true'/>
+            <TextField id="outlined-basic" label="Full name" variant="outlined" margin='normal' fullWidth='true'/>
+            <Button variant="outlined" fullWidth={true} size='large' color='primary' component='label' className={classes.margin}>Contained
+            <input type='file' accept='image/*' hidden />
+            </Button>
+            <Typography variant='subtitle1' className={classes.margin}>
+              by signinng up, you agree to our terms policy and cookies policy
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
+            <Button variant="contained" fullWidth={true}>Sign Up</Button>
           </CardActions>
         </Card>
+         <div>
+         <Card sx={{ maxWidth: 345 }} className={classes.card2}>
+              <Typography>Have an account ? Log In</Typography>
+        </Card>
+         </div>
       </div>
     </div>
   );
